e5bd25e0e8fa8f0e144e5de9767d3fa96152565a
Janet Vorobyeva <janet.vorobyeva@gmail.com>
2019-08-21 02:14:27 -0400

    Style changes to asm_1.c

--- asm_1.c
+++ asm_1.c
@@ -28,21 +28,26 @@ int asm_1() {
     int c;
     long offset = 0; //in dwords
     int curr_num = 0;
-    int line_no = 1;
+    int line_no = 0;
     int column = 0;
     char temp;
 
 
     //main loop: walks until it finds a number, then jumps to read_number
-    while(1) {
+next_line:
+        line_no++;
+        column = 0;
+next_char:
         column++;
         c = getchar();
+
+        //fprintf(stderr, "Line: %d col:%d\n", line_no, column);
         
 //handle current char without getting new one
 handle_char:
         if(c == EOF) { goto end_input; }
 
-        if(c == ' ') { continue; }
+        if(c == ' ') { goto next_char; }
         if(c == '\n') { goto next_line; }
 
         if(c == '#') { goto drop_comment; }
@@ -51,19 +56,32 @@ handle_char:
         if(c <= '9') { goto read_number; }
         if(c < 'A')  { goto invalid_char; }
         if(c <= 'F') { goto read_number; }
+        if(c < 'a')  { goto invalid_char; }
+        if(c <= 'f') { goto read_number; }
+        goto invalid_char;
 
         //consumes numbers as long as it can, then outputs dword
 read_number:
         if(c < '0')  { goto finish_num; }
-        if(c <= '9') { c -= '0'; goto shift_up;}
+        if(c <= '9') { goto num_dig; }
         if(c < 'A')  { goto finish_num; }
-        if(c <= 'F') { c -= 'A' - 10; goto shift_up; }
+        if(c <= 'F') { goto num_uc; }
         if(c < 'a')  { goto finish_num; }
-        if(c <= 'f') { c -= 'a' - 10; goto shift_up; }
-        goto invalid_char;
+        if(c <= 'f') { goto num_lc; }
+        goto finish_num;
+
+num_dig:
+        c -= '0';
+        goto shift_up;
+num_uc:
+        c -= 'A' - 10;
+        goto shift_up;
+num_lc:
+        c -= 'a' - 10;
+        goto shift_up;
 
         //shifts up, adds to curr_num, gets next char
-        shift_up:
+shift_up:
             // <<4 == *2, *2, *2, *2
             curr_num = curr_num + curr_num;
             curr_num = curr_num + curr_num;
@@ -96,11 +114,6 @@ drop_comment:
             return 1;
         }
 
-next_line:
-        line_no++;
-        column = 0;
-        continue;
-    }
 
     invalid_char:
         fprintf(stderr, "ERROR: invalid char '%c' (%d), at line %d, col %d\n", c, c, line_no, column);
